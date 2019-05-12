59ed084884410cb45d96d56a256ada88e79001a3
diinoprp <diinoprp@gmail.com>
2019-05-12 12:02:40 -0300

    Build

---
+++ frontend/build/precache-manifest.7173f5a2146012e55355a5e75866888b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fdd6a9c776eee03e063976c37c0ea28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7413f443b8633d920f",
+    "url": "/static/css/main.5a7718d3.chunk.css"
+  },
+  {
+    "revision": "25c53bba0c35d25232bf",
+    "url": "/static/js/2.302d5a57.chunk.js"
+  },
+  {
+    "revision": "7a7413f443b8633d920f",
+    "url": "/static/js/main.c5aaabe7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
